60fc98be9acb53e2c11eb906ab31238d36ccf20e
UNSTOP4BLE <88965856+UNSTOP4BLE@users.noreply.github.com>
2025-03-06 09:05:04 +0100

    remove some crap

--- build.sh
+++
@@ -1,3 +0,0 @@
-rm -r build
-/opt/devkitpro/portlibs/3ds/bin/arm-none-eabi-cmake -S . -B build
-cmake --build ./build
--- buildlaunch.sh
+++
@@ -1,4 +0,0 @@
-rm -r build
-/opt/devkitpro/portlibs/3ds/bin/arm-none-eabi-cmake -S . -B build
-cmake --build ./build
-'/Users/bruno/Desktop/citra-emu/citra' ./build/test.3dsx
